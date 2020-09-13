e31b537f53e2a16e16310511df36a4a22de8ff2b
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-09-14 02:16:25 +0530

    Search element in Linked List approach-1

---
+++ Geeks4Geeks/LinkedListSearchX.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+	int data;
+    Node* next;
+};
+Node* head=NULL;
+void createNode(int data){
+	Node* newNode=new Node();
+    newNode->data=data;
+	if(head==NULL){
+		head=newNode;
+		return;
+	}
+   newNode->next=head;
+   head=newNode;
+}
+bool searchXIterative(Node* head,int x){
+   Node* temp=head;
+   while(temp!=NULL){
+     if(temp->data==x)
+     	return true;
+    temp=temp->next; 
+   }
+   return false;
+}
+bool searchXRecursive(Node* head,int x){
+  if(head==NULL)
+  	  return false;
+  else
+     if(head->data==x)
+         return true;
+      else
+        return searchXRecursive(head->next,x);
+}
+
+
+int main(){
+int T;
+cin>>T;
+while(T--){
+int data;	
+cin>>data;
+createNode(data);
+}
+int data;
+cin>>data;
+if(searchXIterative(head,data))
+	cout<<"Present"<<endl;
+else
+	cout<<"Not Present"<<endl;
+
+ searchXRecursive(head,data) ? cout<<"Present" : cout<<"Not present"; 
+return 0;
+}
