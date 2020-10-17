f7a3b81daec4ab5059c6ba417798bd69061e8b1b
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-10-17 23:06:14 +0530

    Doubly Linked List Front Inserting Node at End

---
+++ Geeks4Geeks/DLL_EndInsertion.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+ public:
+ Node* prev;
+ Node* next;
+ int data;
+ Node(int data){
+ this->data=data;
+  prev=NULL;
+  next=NULL;
+ }
+};
+
+void createNodeEnd(Node** head,int data){
+ Node* new_node=new Node(data);
+ 
+ if(*head==NULL){
+  *head=new_node;
+  return;
+ }
+
+ Node* temp=*head;
+ while(temp->next!=NULL){
+ 	temp=temp->next;
+ }
+
+  temp->next=new_node;
+  new_node->prev=temp;
+
+}
+void createNodeFront(Node** head,int data){
+ Node* new_node=new Node(data);
+ 
+ if(*head==NULL){
+  *head=new_node;
+  return;
+ }
+
+ new_node->next=*head;
+ (*head)->prev=new_node;
+ *head=new_node;
+
+}
+void display(Node** head){
+ 
+  Node* temp=*head;
+  while(temp!=NULL){
+  	cout<<temp->data<<" ";
+  	temp=temp->next;
+  }
+}
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);	
+
+Node* head=NULL;
+int t;
+cin>>t;
+while(t--){
+ int data;
+ cin>>data;
+ createNodeEnd(&head,data);
+}
+display(&head);
+return 0;	
+}
